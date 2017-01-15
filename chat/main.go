ab660f8b06ffd3390253444ec7824f53b790b9e2
DavyJ0nes <davy@forgesp.com>
2017-01-15 11:44:55 +0000

    Serving HTML from tempaltes

--- chat/main.go
+++ chat/main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
+
+// templateHandler satisfies the http.Handler interface
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	tmpl     *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.tmpl = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.tmpl.Execute(w, nil)
+}
 
 func main() {
 	// create default route handler that displays some static html
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-		<html>
-		  <head>
-			  <title>Chatterbox</title>
-      </head>
-			<body>
-			  <h1>Chatterbox</h1>
-			</body>
-		</html>
-	  `))
-	})
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// Start Web Server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
